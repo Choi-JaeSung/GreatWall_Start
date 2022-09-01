43d4dd9e9043d23ea983ab9f15a1c19273c59fa6
JaeSung_Choi <45025591+Choi-JaeSung@users.noreply.github.com>
2022-09-02 05:04:19 +0900

    Add files via upload

---
+++ Dialogs/FAQDialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading;
+using QnAMakerDialog.Models;
+using QnAMakerDialog;
+
+
+namespace GreatWall.Dialogs
+{
+    [Serializable]
+    [QnAMakerService("https://labuser25qnaapp.azurewebsites.net/qnamaker", "b282c8b6-4518-476e-aff5-98ad7e45dc8e", "568dd922-0c0b-4590-bb1d-af38632a6647", MaxAnswers=5)]
+    public class FAQDialog : QnAMakerDialog<string>
+    {
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            await context.PostAsync($"Sorry, I couldn't find an answer for '{originalQueryText}'.");
+            context.Wait(MessageReceived);
+        }
+
+        public override async Task DefaultMatchHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            if (originalQueryText == "그만")
+            {
+                context.Done("");
+                return;
+            }
+
+            await context.PostAsync(result.Answers.First().Answer);
+
+            context.Wait(MessageReceived);
+        }
+
+        [QnAMakerResponseHandler(0.5)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            var messageActivity = ProcessResultAndCreateMessageActivity(context, ref result);
+            messageActivity.Text = $"I found an answer that might help...{result.Answers.First().Answer}.";
+            await context.PostAsync(messageActivity);
+
+            context.Wait(MessageReceived);
+        }
+    }
+}
